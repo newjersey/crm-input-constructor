22ff8c86d208c37898567270ce501a74b360094d
Ross Dakin <rossdakin@gmail.com>
2020-04-27 12:22:17 -0400

    Use proper CSV parser

--- scripts/process_taxation_output.py
+++ scripts/process_taxation_output.py
@@ -2,6 +2,9 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import csv
 import sys
+import re
+
+naics_re = re.compile('^\d{6}$')
 
 if len(sys.argv) == 1:
   print "Usage:", sys.argv[0], "<input file from taxation>"
@@ -10,22 +13,22 @@ if len(sys.argv) == 1:
 csvwriter = csv.writer(sys.stdout)
 csvwriter.writerow(['EIN', 'AppNum', 'EDA Name', 'Taxation NAME', 'NAICS CODE', 'Clean Indicator', 'Name Confidence'])
 
-with open(sys.argv[1], 'r') as reader:
-    line = reader.readline()
-
-    while line != '':  # The EOF char is an empty string
-        parts = line.split("|")
+with open(sys.argv[1], 'r') as csvfile:
+    readCSV = csv.reader(csvfile)
+    n = 1
+    for row in readCSV:
+        if n == 1:
+          n += 1
+          continue
 
         # 40% and above confidence is about right
-        name_confidence = fuzz.ratio(parts[2].strip().upper(),
-                                     parts[3].strip().upper())
+        name_confidence = fuzz.ratio(row[2].strip().upper(),
+                                     row[3].strip().upper())
 
-        csvwriter.writerow([parts[0].strip(),
-                            parts[1].strip(),
-                            parts[2].strip(),
-                            parts[3].strip(),
-                            parts[4].strip(),
-                            parts[5].strip(),
+        csvwriter.writerow([row[0].strip(),
+                            row[1].strip(),
+                            row[2].strip(),
+                            row[3].strip(),
+                            row[4].strip() if naics_re.match(row[4].strip()) else "",
+                            row[5].strip(),
                             name_confidence])
-
-        line = reader.readline()
