2ee8d1de00a418b4c08d924237a2d2b83c5b5463
Ross Dakin <rossdakin@gmail.com>
2020-05-23 23:50:38 -0400

    Split FICO spreadsheet into tiny ones

---
+++ loans/split-fico.js
@@ -0,0 +1,72 @@
+const chalk = require('chalk');
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+const fs = require('fs');
+const XLSX = require('xlsx');
+
+const SHEET_NAME = 'Sheet1';
+
+const optionDefinitions = [
+  {
+    name: 'src',
+    alias: 's',
+    type: String,
+    defaultOption: true,
+    description: '(REQUIRED) Source CSV input file; see sample.csv',
+  },
+  {
+    name: 'dir',
+    alias: 'd',
+    type: String,
+    description: '(REQUIRED) Output directory',
+  },
+];
+
+function printUsage() {
+  const usage = commandLineUsage([
+    {
+      header: 'FICO Output Splitter',
+      content:
+        'This takes an XLSX file and produces N new files in the designated path, each corresponding to a row.',
+    },
+    {
+      header: 'Options',
+      optionList: optionDefinitions,
+    },
+    {
+      content: 'Example: npm run loans loans/sample.xlsx -- -p',
+    },
+  ]);
+
+  console.log(usage);
+}
+
+function main() {
+  const options = commandLineArgs(optionDefinitions);
+
+  if (!(options.src && options.dir)) {
+    printUsage();
+    return;
+  }
+
+  const workbook = XLSX.readFile(options.src, { type: 'file' });
+  const worksheet = workbook.Sheets[SHEET_NAME];
+  rows = XLSX.utils.sheet_to_json(worksheet);
+
+  console.log(
+    `Splitting ${chalk.blue(options.src)} into ${rows.length} spreadsheets in ${chalk.blue(options.dir)}`
+  );
+
+  rows.forEach(row => {
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.json_to_sheet([row]);
+    const path = `${options.dir}/FICO_CV19L${row.CustomerKey}.xlsx`;
+
+    wb.SheetNames.push(SHEET_NAME);
+    wb.Sheets[SHEET_NAME] = ws;
+
+    XLSX.writeFile(wb, path);
+  });
+}
+
+main();
